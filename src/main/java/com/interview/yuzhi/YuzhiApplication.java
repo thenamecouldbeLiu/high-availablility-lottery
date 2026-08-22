6868f8832590317fff433432501707655fab6df0
thenamecouldbeLiu <pppoe4016@gmail.com>
2026-08-22 09:42:50 +0800

    inti

---
+++ src/main/java/com/interview/yuzhi/YuzhiApplication.java
@@ -0,0 +1,13 @@
+package com.interview.yuzhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuzhiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuzhiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interview/yuzhi/YuzhiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.yuzhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuzhiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
